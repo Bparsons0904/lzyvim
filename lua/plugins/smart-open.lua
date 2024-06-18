da7d4c8025d655465649722775dfeecd7d99f54b
Bob Parsons <deadstylebp@gmail.com>
2024-06-18 18:20:16 -0500

    init

---
+++ lua/plugins/smart-open.lua
@@ -0,0 +1,15 @@
+return {
+  "danielfalk/smart-open.nvim",
+  branch = "0.2.x",
+  config = function()
+    require("telescope").load_extension("smart_open")
+  end,
+  dependencies = {
+    "kkharji/sqlite.lua",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    { "nvim-telescope/telescope-fzy-native.nvim" },
+  },
+  vim.keymap.set("n", "<leader><leader>", function()
+    require("telescope").extensions.smart_open.smart_open()
+  end, { noremap = true, silent = true }),
+}
